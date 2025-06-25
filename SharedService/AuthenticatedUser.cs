8126ddc91904f9f8cfaa5ac42693762f911faa92
osama541 <ro6760644@gmail.com>
2025-06-25 04:47:21 -0700

    Add files via upload

---
+++ SharedService/AuthenticatedUser.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Security.Claims;
+
+namespace WebApi.SharedServices
+{
+    public class AuthenticatedUser : IAuthenticatedUser
+    {
+        public AuthenticatedUser(IHttpContextAccessor httpContextAccessor)
+        {
+            UserId = httpContextAccessor.HttpContext.User.FindFirstValue("uid");
+        }
+
+        public string UserId { get; set; }
+    }
+}
